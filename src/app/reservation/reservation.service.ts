f4d55de36e7dcff7ef367fdd928c0ac17f6ddcb3
alfredryangerero <alfredrrg-v@crimsonlogic.com>
2025-05-29 08:19:49 +0800

    remove localstorage as a storage of data

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,15 +8,6 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
-  constructor() {
-    // Load reservations from localStorage if available
-    //const storedReservations = localStorage.getItem("reservations");
-    let storedReservations = localStorage.getItem("reservations");
-    if (storedReservations) {
-      this.reservations = JSON.parse(storedReservations);
-    }
-  }
-
   //CRUD
   getReservations(): Reservation[]{
     return this.reservations;
@@ -29,20 +20,16 @@ export class ReservationService {
   addReservation(reservation: Reservation): void{
     reservation.id=Date.now().toString()
     this.reservations.push(reservation);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void{
     let index=this.reservations.findIndex(res=> res.id === id);
     this.reservations.splice(index,1);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string,updatedReservation: Reservation): void{
     let index=this.reservations.findIndex(res=> res.id === id);
     updatedReservation.id=id
-    // console.log('update:' + updatedReservation.id);
     this.reservations[index] = updatedReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 }
